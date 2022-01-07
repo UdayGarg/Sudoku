4733f398456fe6f4004cb924647c2fa84b216b8c
Uday garg <65303126+UdayGarg@users.noreply.github.com>
2022-01-07 14:08:42 +0530

    changes to creation of solved sudoku

--- input_solved_sudoku.py
+++ input_solved_sudoku.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import numpy as np
+import access_file
 
 
 def create_nparray(strr):
@@ -17,12 +18,24 @@ def create_nparray(strr):
         arr.append(sub)
     return np.array(arr)
 
+
 def pre_check(string):
     return string.isdigit()
 
 
-solved = "346179258187523964529648371965832417472916835813754629798261543631485792254397186"
+"""solved = "346179258187523964529648371965832417472916835813754629798261543631485792254397186"
 if pre_check(solved):
     n = create_nparray(solved)
     print(n)
-else: print("invalid ")
+else:
+    print("invalid ")"""
+
+if __name__ == "__main__":
+    num = input("How many sudokus do you want to create: ")
+    ls = access_file.return_solutions("sudoku.csv", "solutions", int(num))
+    for items in ls:
+        if pre_check(items):
+            n = create_nparray(items)
+            print(n)
+        else:
+            print("invalid ")
