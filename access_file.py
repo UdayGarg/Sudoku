b481cdd99884cf70568cdf4e717273292d005c3f
Uday garg <65303126+UdayGarg@users.noreply.github.com>
2022-01-07 13:45:14 +0530

    Added read csv file

---
+++ access_file.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def return_solutions(filename, col, n):
+    data = pd.read_csv(filename)
+    ls = data[col].tolist()
+    new_ls = []
+    for i in range(n):
+        new_ls.append(ls[i])
+    return new_ls
+
+
+if __name__ == "__main__":
+    print(return_solutions("sudoku.csv", "solutions", 3)) # to dynamically assign solutions to create a data set
+
