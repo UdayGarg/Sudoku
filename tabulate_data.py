d444ff8e06c78d71633a8105bcf7ac17ea998543
Uday garg <65303126+UdayGarg@users.noreply.github.com>
2022-02-21 22:10:15 +0530

    1st attampt

---
+++ tabulate_data.py
@@ -0,0 +1,20 @@
+"""The function of this module is to form and print table from array to publish suitable looking SUDOKU
+puzzles"""
+
+from tabulate import tabulate
+import functions as func
+
+
+def tabulate_data(item):
+    """Function to create tables from array using external module "tabulate" """
+    #  tb = (tabulate(item, tablefmt="fancy_grid"))
+    tb = (tabulate(item, tablefmt="html"))
+    return tb
+
+
+if __name__ == "__main__":
+    ls = func.create_list_of_grids()
+    for data in ls:
+        print(tabulate_data(data))
+
+
