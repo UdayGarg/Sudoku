a1a6059ec59c102a33a0bae47e0db75edb1cc9f9
Uday garg <65303126+UdayGarg@users.noreply.github.com>
2022-02-21 16:08:48 +0530

    Added docstrings and comments

--- functions.py
+++ functions.py
@@ -3,11 +3,10 @@
 import pandas as pd
 import numpy as np
 
-"""return_solutions() will read the user specified csv filename and column and return a list of items f
-rom that column """
-
 
 def return_solutions(filename, col, number_of_items):
+    """return_solutions() will read the user specified csv filename and column and return a list of items
+    from that column """
     data = pd.read_csv(filename)  # reading the csv file using pandas
     ls = data[col].tolist()  # converting the whole df to a list
     new_ls = []
@@ -16,10 +15,8 @@ def return_solutions(filename, col, number_of_items):
     return new_ls
 
 
-"""create_nparray from list of strings provided by return_solutions"""
-
-
 def create_nparray(strr):
+    """create_nparray from list of strings provided by return_solutions"""
     wd = []
     for ch in strr:
         wd.append(ch)  # converting the string to a list
@@ -34,10 +31,8 @@ def create_nparray(strr):
     return np.array(arr).astype(np.int32)  # type converting the array to an 2d np array with int32 type
 
 
-"""creates a list of sudoku grids to ease use later"""
-
-
 def create_list_of_grids():
+    """creates a list of sudoku grids to ease use later"""
     ls_grids = []
     #   num = input("How many sudokus do you want to create: ")
     ls = return_solutions("sudoku.csv", "solutions", 10)  # It will return a list of 10 sudoku strings
@@ -50,35 +45,27 @@ def create_list_of_grids():
     return ls_grids
 
 
-"""simple pre check function to check if all the values in sudoku are digits"""
-
-
 def pre_check(string):
+    """simple pre check function to check if all the values in sudoku are digits"""
     return string.isdigit()
 
 
-"""splitter function to return rows of sudoku"""
-
-
 def split_into_rows(grid):
+    """splitter function to return rows of sudoku"""
     rows = grid[0:, :]
     return rows
 
 
-"""splitter function to return columns of sudoku"""
-
-
 def split_into_cols(grid):
+    """splitter function to return columns of sudoku"""
     cols = []
     for col in range(9):
         cols.append(grid[:, col])
     return cols
 
 
-"""splitter function to return 3x3 subgrids of sudoku"""
-
-
 def split_into_subgrids(grid):
+    """splitter function to return 3x3 subgrids of sudoku"""
     subgrids = []
     for box_i in range(3):
         for box_j in range(3):
@@ -90,48 +77,36 @@ def split_into_subgrids(grid):
     return np.array(subgrids)
 
 
-"""trial of simple test to make code simpler"""
-"""This can be ignored for now as it hold little to no significance"""
-
-
 def simple_test(test_item):
-    if np.all(np.sort(test_item, axis=-1) == np.arange(1, 10), axis=-1):
-        return True
-    else:
-        return False
-
-
-"""testing function that check if the values in a test item (row, col or subgrid) are within range, 
-i.e 1 to 9"""
+    """trial of simple test to make code simpler
+    This can be ignored for now as it hold little to no significance"""
+    return np.all(np.sort(test_item, axis=-1) == np.arange(1, 10), axis=-1)
 
 
 def val_range(test_item):
-    c = 0
-    for i in test_item:
-        if 1 <= i <= 9:
-            c = c + 1
-    if c != 9:
+    """testing function that check if the values in a test item (row, col or subgrid) are within
+    range,i.e 1 to 9"""
+    count = 0
+    for val in test_item:
+        if 1 <= val <= 9:
+            count = count + 1
+    if count != 9:
         return False
-    else:
-        return True
-
-
-"""Testing function to check if all the vlues in a test item are unique, i.e without duplicates"""
 
 
 def unique(test_item):
-    if len(set(test_item)) == 9:  # conversion to set only hold unique values and if its len is 9 then we
-        # know that the valus inside the test item are unique
-        return True
-    else:
-        return False
+    """Testing function to check if all the values in a test item are unique,
+    i.e without duplicates"""
+    return len(set(test_item)) == 9
+    # conversion to set only hold unique values and if its len is 9 then we
+    # know that the valus inside the test item are unique
 
 
 if __name__ == "__main__":
     ls = create_list_of_grids()
     for item in ls:  # for grids in list of grids
+        print(item)
         for i in split_into_rows(item):  # split grid in to rows and check if they are valid
-            print(i)
             if val_range(i) and unique(i):
                 pass
             else:
@@ -142,7 +117,8 @@ if __name__ == "__main__":
                 pass
             else:
                 break
-        print("Cols checked")  # return a confirmation ensuring all cols are checked
+        print("Cols checked")
+        # return a confirmation ensuring all cols are checked
         for i in split_into_subgrids(item):  # split grid in to 3x3 subgrids and check if they are valid
             if val_range(i) and unique(i):
                 pass
