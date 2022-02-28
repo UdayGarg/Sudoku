1fcf4e0d9fb5882abee87dd6ced5025b712842f9
uday <uday@gmail.com>
2022-02-28 23:13:27 +0530

    more sample images

--- text_to_image.py
+++ text_to_image.py
@@ -1,10 +1,14 @@
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
+from random import randint
 
 source_img = Image.open("demo.png").convert("RGBA")
-fontsize = 5
+fontsize = 40
 draw = ImageDraw.Draw(source_img)
-draw.rectangle(((10,10), (60, 60)), outline="black", width = 2)
-font = ImageFont.truetype("Ubuntu-R.ttf", 25)
-draw.text((20,20), "3",fill=(0,0,0), font=font)
-
-source_img.save("output1.png")
+draw.rectangle(((10,10), (60, 60)), outline="black", width = 6)
+font = ImageFont.truetype("Ubuntu-R.ttf", fontsize)
+draw.text((15,15), "9",fill=(0,0,0), font=font)
+width = randint(0,10)
+height = randint(11,100)
+filename = "grid-{}-{}.png".format(width, height)
+print("Saving {}".format(filename))
+source_img.save(filename)
