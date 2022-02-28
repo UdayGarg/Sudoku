801ff2f8d226c20e3b94432a8fe0bce7cdc6b00e
uday <uday@gmail.com>
2022-02-28 18:20:03 +0530

    modification to text_to_image.py

--- text_to_image.py
+++ text_to_image.py
@@ -1,9 +1,10 @@
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 
 source_img = Image.open("demo.png").convert("RGBA")
-
+fontsize = 5
 draw = ImageDraw.Draw(source_img)
-draw.rectangle(((50,50), (400, 400)), fill = "black")
-draw.text((300,300), "3", font=ImageFont.load_default())
+draw.rectangle(((10,10), (60, 60)), outline="black", width = 2)
+font = ImageFont.truetype("Ubuntu-R.ttf", 25)
+draw.text((20,20), "3",fill=(0,0,0), font=font)
 
 source_img.save("output1.png")
