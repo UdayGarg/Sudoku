6423b2c309a1043ef749bd557b675b2f9714c7f2
Uday garg <65303126+UdayGarg@users.noreply.github.com>
2022-03-19 19:00:28 +0530

    Changes to trials.py

--- trials.py
+++ trials.py
@@ -3,9 +3,14 @@ from PIL import Image, ImageFont, ImageDraw
 
 
 font = ImageFont.truetype("Arial Unicode.ttf", 40)
-strr = func.return_solutions("sudoku.csv", "solutions", 1)
+sudoku = str(func.return_solutions("sudoku.csv", "solutions", 1))
 img = Image.open("/Users/anuj/Desktop/Sudoku/Images/grid-1646985520.479167.png")
 draw = ImageDraw.Draw(img)
-for item in strr:
-    draw.text((10,10), item, font = font)
+print(type(sudoku))
+
+for item in range(len(sudoku)):   
+    for x in range(50, 900, 100):
+        for y in range(50, 900, 100):
+            draw.text((x,y), sudoku[item], font = font)
+
 img.save("Sudoku1.png")
