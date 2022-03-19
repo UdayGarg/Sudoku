830402d578c5d003409563ee646dbe9da7ebf90c
Uday garg <65303126+UdayGarg@users.noreply.github.com>
2022-03-19 22:03:41 +0530

    fixed trial.oy

--- trials.py
+++ trials.py
@@ -9,8 +9,8 @@ draw = ImageDraw.Draw(img)
 print(sudoku)
 print(len(sudoku))
 count = 0
-for x in range(0, 900, 100):
-    for y in range(0, 900, 100):
+for x in range(25, 900, 100):
+    for y in range(50, 900, 100):
         draw.text((y,x), sudoku[count], font = font)
         count +=1
 
