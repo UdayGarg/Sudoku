ca6649e336c27a7b78760c94844b8007f8c6fde6
Uday garg <65303126+UdayGarg@users.noreply.github.com>
2022-03-19 21:09:44 +0530

    fixed trial.py

--- trials.py
+++ trials.py
@@ -2,15 +2,17 @@ import functions as func
 from PIL import Image, ImageFont, ImageDraw
 
 
-font = ImageFont.truetype("Arial Unicode.ttf", 40)
-sudoku = str(func.return_solutions("sudoku.csv", "solutions", 1))
+font = ImageFont.truetype("Arial Unicode.ttf", 50)
+sudoku = func.return_solutions("sudoku.csv", "solutions", 1)[0]
 img = Image.open("/Users/anuj/Desktop/Sudoku/Images/grid-1646985520.479167.png")
 draw = ImageDraw.Draw(img)
-print(type(sudoku))
+print(sudoku)
+print(len(sudoku))
+count = 0
+for x in range(0, 900, 100):
+    for y in range(0, 900, 100):
+        draw.text((y,x), sudoku[count], font = font)
+        count +=1
 
-for item in range(len(sudoku)):   
-    for x in range(50, 900, 100):
-        for y in range(50, 900, 100):
-            draw.text((x,y), sudoku[item], font = font)
 
 img.save("Sudoku1.png")
